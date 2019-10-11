a2e25358df7c6a450b8d22ebb621853b1e7ab751
Florian Lecocq <florian.lecocq@outlook.com>
2019-10-11 16:04:56 +0200

    Change the structure (the repo is the package itself)

--- Old Packages/Packages/PoissonRMoE/R/SoTh.R
+++
@@ -1,7 +0,0 @@
-SoTh = function(x, lambda)
-{
-  if (lambda==0) return (x)
-  if (x > lambda) return (x-lambda)
-  if (x < -lambda) return (x+lambda)
-  return (0)
-}
--- Old Packages/Packages/RMoE/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(RMoE)
-
-test_check("RMoE")
